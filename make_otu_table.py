a4f5d6023f11a81fe0c19f4e8aa907795e8e885b
root <martenhoogeveen@gmail.com>
2018-04-16 11:50:59 +0200

    Change header in output

--- make_otu_table.py
+++ make_otu_table.py
@@ -99,7 +99,7 @@ def remove_single_clusters(outputFolder):
         for record in SeqIO.parse(outputFolder+"/otu_sequences_vsearch.fa", "fasta"):
             size = str(record.description).split("size=")[1][:-1]
             if int(size) > int(args.clustersize):
-                otuFile.write(">"+str(record.description)+"\n"+str(record.seq)+"\n")
+                otuFile.write(">"+str(record.id)+"\n"+str(record.seq)+"\n")
                 otuCount += 1
             else:
                 singletonCount += 1

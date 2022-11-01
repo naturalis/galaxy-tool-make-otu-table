d76f5385bbdd96e78aaa6472dd66ffaad1b5f55d
dickgroenenberg <dickgroenenberg@gmail.com>
2022-11-01 15:27:11 +0100

    use env python; correct tool_dir

--- make_otu_table.py
+++ make_otu_table.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 """
 
 """
@@ -152,7 +152,7 @@ def dada2_cluster(outputFolder):
                 output.write(record.format("fastq"))
     admin_log(outputFolder, out="Sequences with N bases found and removed: "+str(ncount), function="remove N bases")
 
-    out, error = Popen(["Rscript", "/home/galaxy/Tools/galaxy-tool-make-otu-table/dada2.R", outputFolder + "/combined_dada_filtered.fastq", outputFolder + "/otu_sequences.fa"], stdout=PIPE, stderr=PIPE).communicate()
+    out, error = Popen(["Rscript", "/srv/galaxy/local_tools/galaxy-tool-make-otu-table/dada2.R", outputFolder + "/combined_dada_filtered.fastq", outputFolder + "/otu_sequences.fa"], stdout=PIPE, stderr=PIPE).communicate()
     admin_log(outputFolder, out=out.decode(), error=error.decode(), function="dada2")
 
 def usearch_otu_tab(outputFolder):
